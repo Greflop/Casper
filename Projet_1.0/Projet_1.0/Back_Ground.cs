bf778e0d5e19a50cd78617e209f248f90928cadb
Uranium2 <antoine.tavernier@epita.fr>
2014-03-10 15:28:23 +0100

    added back ground

---
+++ Projet_1.0/Projet_1.0/Back_Ground.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Projet_1._0
+{
+    class Back_Ground
+    {
+        Texture2D Back_GroundTexture;
+        public Back_Ground(ContentManager content)
+        {
+            Back_GroundTexture = content.Load<Texture2D>("BackGround");
+        }
+        public void Update()
+        {
+            // Pour l instant le fond est fixe
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Back_GroundTexture, new Rectangle(0, 0, 1680, 1050), Color.White);
+        }
+    }
+}
